7e50a84211c3fe4eb22be832bfb4e12e14f14815
1willcobb <cobb.will@gmail.com>
2023-07-10 23:58:58 -0700

    layed out further files

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require('inquirer')
+
+//TODO - PROMPT i can enter up to 3 characters for the text for my SVG
+
+//TODO - Shape with LIST of shapes: circle, triangle, square
+
+//TODO - prompted for text color: i can respond with color name OR hex number
+
+//TODO - at the end an SVG file is generated with title 'logo.svg'
+
+//TODO - command line statement stating "generated logo.svg"
+
+//TODO when i open the file i am shown a 300x200 pixel images that matches what i went through
+
