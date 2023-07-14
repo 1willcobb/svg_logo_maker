5d19ad3cc0b131319d5c3aac323a596c92f22730
1willcobb <cobb.will@gmail.com>
2023-07-14 07:36:39 -0700

    made circel class. started working through inqury but need to put them in the right oder

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const inquirer = require('inquirer')
 //const shape = require('./lib/shape')
 //const square = require('./lib/square')
-//const circle = require('./lib/circle')
+const Circle = require('./lib/circle')
 //const triangle = require('./lib/triangle')
 const fs = require('fs')
+const { error } = require('console')
+
+
 
 //TODO - PROMPT i can enter up to 3 characters for the text for my SVG 
 const intro_questions = [
@@ -12,21 +15,12 @@ const intro_questions = [
         name: 'chars',
         message: 'Enter up to 3 chars to represent my logo'
     },
-    {
-        name: 'text_color',
-        message: 'Text color (Color name or hex number)'
-    },
-    {
-        name: 'shape_choice',
-        message: 'Choose a shape:',
-        choices: ['circle', 'triangle', 'square'],
-        type: 'list'
-    }
+
 ]
 
 
 
-const svg = (txt, shape, bg_color) => {
+const createSVG = (txt, shape, bg_color) => {
     return (`
     <svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <rect x="0" y="0" width="300" height="200" fill="${bg_color}"/>
@@ -36,24 +30,76 @@ const svg = (txt, shape, bg_color) => {
     `)
 }
 
+const chooseShape = (shape, shapeColor) => {
+    if (shape === 'circle') {
+        return new Circle(shapeColor)
+    } else if (shape === 'square') {
+        return new Square(shapeColor)
+    } else if (shape === 'triangle') {
+        return new Triangle(shapeColor)
+    } else {
+        return "not a valid shape"
+    }
+}
+
+
 const init = () => {
     inquirer
         .prompt([
             {
                 type: 'confirm',
                 name: 'welcome',
-                message: 'Welcome to the SVG logo generator.\ndo you want to make your own SVG?'
+                message: 'Welcome to the SVG logo generator.\nDo you want to make your own SVG?'
             },
+        ])
+        .then(({ welcome }) => {
+            if (welcome) {
+                console.log(welcome)
+                console.log("okay, bye for now")
+                prompt.ui.close()
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            console.log("not an option")
+        })
+        .then(() => {
+            inquirer
+        .prompt([
             {
                 name: 'chars',
-                message: 'Enter up to 3 chars to represent my logo'
+                message: 'Enter up to 3 chars to represent my logo: '
+            },
+            {
+                name: 'textColor',
+                message: 'Enter text color (name a color or a hex number 0x00FFAA): '
+            },
+            {
+                name: 'shapeChoice',
+                message: 'Choose a shape:',
+                choices: ['circle', 'triangle', 'square'],
+                type: 'list'
+            },
+            {
+                name: 'shapeColor',
+                message: 'Enter the shape color (name a color or a hex number 0x00FFAA): '
             }
+
         ])
-        .then(() => {
-            
-            fs.writeFileSync('svg.svg', user_svg)
-            
+        .then(({ chars, textColor, shapeChoice, shapeColor }) => {
+            const userShape = chooseShape(shapeChoice, shapeColor)
+            console.log(userShape.render())
+            //fs.writeFileSync('svg.svg', user_svg)
         })
+        .then(() => {
+
+        })
+        .catch((err) => {
+            console.log(err)
+            console.log("hmm... something didn't work...")
+        })
+        })
+    
 }
 
 
